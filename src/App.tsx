8fcb11623ddccfea264b34805706d4886f6514f6
gavincraig <gcraig197@gmail.com>
2023-06-08 22:13:01 +0100

    tailwind setup

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <section style={{ minWidth: "90vw", minHeight: "90vh" }}>
         <h1>Coffee Wheel</h1>
+        <div className="bg-red-200">test</div>
         <Wheel />
       </section>
     </div>
---
+++ src/components/CoffeeDropdown/CoffeeDropdown.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const CoffeeDropdown = (props: Props) => {
+  return (
+    <div className='bg-red-300'>CoffeeDropdown</div>
+  )
+}
+
+export default CoffeeDropdown
