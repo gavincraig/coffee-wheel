a65ad5093f095651b610b1fc8cdc8d97ece3e772
gavincraig <gcraig197@gmail.com>
2023-06-08 21:45:14 +0100

    data & recording flavours

---
+++ src/components/WheelSector/WheelSector.tsx
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { SelectionsContext } from "../../context/SelectionsContext";
+import styles from './WheelSector.module.css';
+
+const WheelSector = ({ onClick, id, breadcrumbIds, children, ...props }) => {
+  const { selections } = useContext(SelectionsContext);
+  const isSelected = selections?.includes(id);
+  return (
+    <g data-isSelected={isSelected} opacity={isSelected ? 1 : 0.5} className={styles.root} onClick={() => onClick(id)}>
+      <path
+        
+        id={id}
+        {...props}
+      />
+      {children}
+    </g>
+  );
+};
+
+export default WheelSector;
---
+++ src/components/WheelSector/index.ts
@@ -0,0 +1,1 @@
+export { default as WheelSector } from './WheelSector';
